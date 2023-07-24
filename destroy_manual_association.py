0a4ea64a015f66c7125dcf8714ca33ad2b968898
Pedro Lemos <pedro.lemos@cedrotech.com>
2023-07-25 00:15:02 +0100

    feat: requests

--- destroy_manual_association.py
+++ destroy_manual_association.py
@@ -1,23 +1,5 @@
-import requests
 import json
+import requests
 
-
-class MakeApiCall:
-
-    def get_data(self, api):
-        response = requests.get(f"{api}")
-        if response.status_code == 200:
-            print("sucessfully fetched the data")
-            self.formatted_print(response.json())
-        else:
-            print(
-                f"Hello person, there's a {response.status_code} error with your request")
-
-    def get_user_data(self, api, parameters):
-        response = requests.get(f"{api}", params=parameters)
-        if response.status_code == 200:
-            print("sucessfully fetched the data with parameters provided")
-            self.formatted_print(response.json())
-        else:
-            print(
-                f"Hello person, there's a {response.status_code} error with your request")
+response = requests.get('https://api.github.com')
+print(response.json())
